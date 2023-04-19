8b7a51e134818a19b2fbf241279d9a98d8d89956
EGGA05 <130729862+EGGA05@users.noreply.github.com>
2023-04-19 09:06:42 -0700

    archivos de la actividad 3

---
+++ script.js
@@ -0,0 +1,4 @@
+function desc(){
+    var auto = document.getElementById("auto").value;
+    window.alert(auto=="focus" ? "el modelo tiene un descuento del %5" : "el modelo tiene un descuento del %10");
+}
